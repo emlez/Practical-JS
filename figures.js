b45e78ddeb7a435849428365c40ef27a21c44eb8
EmlSn <eml.snzrmz@gmail.com>
2021-09-04 00:46:31 -0500

    Changed every console.log to template literals.

--- figures.js
+++ figures.js
@@ -1,10 +1,13 @@
 // Code for square
 console.group("Square 🟨");
 const squareSide = 5;
-console.log("The sides of the square measure: " + squareSide + " cm.");
+console.log(`The sides of the square measure: ${squareSide} cm.`);
+
+const squarePerimeter = squareSide * 4;
+console.log(`The perimeter of the square measures: ${squarePerimeter} cm.`);
 
 const squareArea = squareSide * squareSide;
-console.log("The area of the square measures: " + squareArea + " cm^2");
+console.log(`The area of the square measures: ${squareArea} cm^2.`);
 console.groupEnd();
 
 // Code for triangle

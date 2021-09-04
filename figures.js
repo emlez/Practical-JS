2f264c259e9e2dc8d422cccf5c7ba778243decd8
EmlSn <eml.snzrmz@gmail.com>
2021-09-04 01:13:54 -0500

    Encapsulated code in functions.

--- figures.js
+++ figures.js
@@ -1,55 +1,49 @@
 // Code for square
 console.group("Square 🟨");
-const squareSide = 5;
-console.log(`The sides of the square measure: ${squareSide} cm.`);
 
-const squarePerimeter = squareSide * 4;
-console.log(`The perimeter of the square measures: ${squarePerimeter} cm.`);
+function squarePerimeter(side) {
+  return side * 4;
+}
 
-const squareArea = squareSide * squareSide;
-console.log(`The area of the square measures: ${squareArea} cm^2.`);
+function squareArea(side) {
+  return side * side;
+}
+
 console.groupEnd();
 
 // Code for triangle
 console.group("Triangle ▲");
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-console.log(
-  `The sides of the triangle measure: ${triangleSide1} cm, ${triangleSide2} cm and ${triangleBase} cm.`
-);
 
-const triangleHeight = 5.5;
-console.log(`The height of the triangle measures: ${triangleHeight} cm.`);
+function trianglePerimeter(side1, side2, base) {
+  return side1 + side2 + base;
+}
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-console.log(`The perimeter of the triangle measures: ${trianglePerimeter} cm.`);
+function triangleArea(base, height) {
+  return (base * height) / 2;
+}
 
-const triangleArea = (triangleBase * triangleHeight) / 2;
-console.log(`The area of the triangle measures: ${triangleArea} cm^2.`);
 console.groupEnd();
 
 // Code for circle
 console.group("Circle 🟡");
 
-// Radius
-const circleRadius = 4;
-console.log(`The circle's radius measures: ${circleRadius} cm.`);
-
 // Diameter
-const circleDiameter = circleRadius * 2;
-console.log(`The circle's diameter measures: ${circleDiameter} cm.`);
+function circleDiameter(radius) {
+  return radius * 2;
+}
 
 // Pi
 const Pi = Math.PI;
-console.log(`Pi is: ${Pi}.`);
 
 // Circumference
-const circlePerimeter = circleDiameter * Pi;
-console.log(`The circle's perimeter is: ${circlePerimeter}.`);
+function circlePerimeter(radius) {
+  const diameter = circleDiameter(radius);
+  return diameter * Pi;
+}
 
 // Area
-const circleArea = Pi * (circleRadius * circleRadius);
-console.log(`The circle's area is: ${circleArea} cm^2.`);
+function circleArea(radius) {
+  return radius * radius * Pi;
+}
 
 console.groupEnd();
